d21513b95d83f85444d6279c78909d4fe99bf659
kimjs191009 <kimjs191009@gmail.com>
2026-05-28 14:04:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,18 @@ max_comments = st.sidebar.slider(
 
 def extract_video_id(url):
 
+    patterns = [
+        r"v=([a-zA-Z0-9_-]{11})",
+        r"youtu\.be/([a-zA-Z0-9_-]{11})"
+    ]
+
+    for pattern in patterns:
+        match = re.search(pattern, url)
+
+        if match:
+            return match.group(1)
+
+    return None
 
 patterns = [
     r"v=([a-zA-Z0-9_-]{11})",
